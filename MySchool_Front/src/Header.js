63eeea39faa03721f9b6469bb87280afd9c2aed9
Erblin Gashi <eg52040@ubt-uni.net>
2024-01-25 01:27:36 +0100

    Përmirsime & body added

---
+++ MySchool_Front/src/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './css/open-iconic-bootstrap.min.css';
+import './css/animate.css';
+import './css/owl.theme.default.min.css';
+import './css/magnific-popup.css';
+import './css/aos.css';
+import './css/ionicons.min.css';
+import './css/flaticon.css';
+import './css/icomoon.css';
+import './css/style.css';
+
+
+const Header = () => (
+  <>
+    <meta charSet="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <link
+      href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Fredericka+the+Great"
+      rel="stylesheet"
+    />
+  </>
+);
+
+export default Header;
