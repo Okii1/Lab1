722c85bdb3423f5322a80b56ef9bc93d90cd4c83
Erblin Gashi <eg52040@ubt-uni.net>
2024-02-17 22:01:29 +0100

    Teachers Admin side e permirsuar

--- MySchool_Front/src/TeachersA.js
+++ MySchool_Front/src/TeachersA.js
@@ -7,14 +7,68 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
 const TeachersA = () => {
- 
+  const [showModal, setShowModal] = useState(false);
+  const [newTeacher, setNewTeacher] = useState({
+    Id:0,
+    Name: '',
+    Surname:'',
+    Description: '',
+    CourseId: 0,
+  });
+  
+const handleInputChange = (e) => {
+  const { name, value } = e.target;
+  setNewTeacher((prevTeacher) => ({
+    ...prevTeacher,
+    [name]: value,
+    
+  }));
+};
+const handleModal = () => {
+  setShowModal(!showModal);
+};
+const handleSaveTeacher = async () => {
+  console.log("ID: "+newTeacher.Id);
+  if (!newTeacher.Name) {
+    alert("Please fill in all required fields.");
+    return;
+  }
+  try {
+    if (newTeacher.Id==0) {
+      const response = await axios.post('https://localhost:44361/api/Teachers/AddTeachers', {
+        Name: newTeacher.Name,
+        Surname: newTeacher.Surname,
+        Description: newTeacher.Description,
+        CourseId: newTeacher.CourseId
+        
+      });
+      console.log('New Teacher added:', response.data);
+      window.location.reload();
+
+
+    } else {
+      await axios.put(`https://localhost:44361/api/Teachers/UpdateTeachers`, {
+        Id: newTeacher.Id,
+        Name: newTeacher.Name,
+        Surname: newTeacher.Surname,
+        Description: newTeacher.Description,
+        CourseId: newTeacher.CourseId
+     
+      });
+      console.log('Teacher updated:', newTeacher.id);
+      window.location.reload();
+
+    }
+    
+    setShowModal(false);
+  } catch (error) {
+    console.error('Error saving Teacher:', error);
+  }
+};
   const userRoleId = localStorage.getItem('userRoleId');
   const navigate = useNavigate();
-
-  const [courses, setCourses] = useState([]);
+  const [Teachers, setTeachers] = useState([]);
   const handleLogout = async () => {
-
-
     const url = 'https://localhost:44361/api/Account/Logout';
     try {
     const response = await fetch(url, {
@@ -40,42 +94,115 @@ const TeachersA = () => {
     if (userRoleId == 0 || userRoleId == 2) {
       navigate('/');
     }
-    
-    
+    axios.get('https://localhost:44361/api/Teachers/GetAllTeachers')
+      .then(response => {
+        setTeachers(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error('Error fetching Teachers:', error);
+      });
   }, []); 
-
-  return (
+  const handleDelete = async (TeacherId) => {
+    try {
+      await axios.delete(`https://localhost:44361/api/Teachers/DeleteTeachers/${TeacherId}`);
+      setTeachers((prevTeachers) => prevTeachers.filter(Teacher => Teacher.id !== TeacherId));
+    } catch (error) {
+      console.error(`Error deleting Teacher with ID ${TeacherId}:`, error);
+    }
+  };
+  const handleEdit = async (TeacherId) => {
+    const selectedTeacher = Teachers.find(Teacher => Teacher.id === TeacherId);
+    console.log("selectedTeacher:", selectedTeacher);
+    setNewTeacher({
+      Id: selectedTeacher.id,
+      Name: selectedTeacher.name,
+      Surname: selectedTeacher.surname,
+      Description: selectedTeacher.description,
+      CourseId: selectedTeacher.courseId
     
+    });
+  setShowModal(true);
+  };
+  return (
     <>
        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
-	    <div className="container d-flex align-items-center">
-	    	<a className="navbar-brand" href="/Home">MySchool</a>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span className="oi oi-menu"></span> Menu
-	      </button>
-	      <div className="collapse navbar-collapse" id="ftco-nav">
-	        <ul className="navbar-nav ml-auto">
-	        	<li className="nav-item "><a href="/Home" className="nav-link pl-0">Home</a></li>
-				<li className="nav-item "><a href="/CoursesA" className="nav-link">Courses</a></li>
-	        	<li className="nav-item active"><a href="/TeachersA" className="nav-link">Teachers</a></li>
-	        	<li className="nav-item"><a href="/Pricing" className="nav-link">Pricing</a></li>
-	            <li className="nav-item"><a href="/Contact" className="nav-link">Contact</a></li>
-				<li className="nav-item"><a href="#" className="nav-link" onClick={handleLogout}>Logout</a></li>
-	        </ul>
-	      </div>
-	    </div>
-	  </nav>
+      <div className="container d-flex align-items-center">
+        <a className="navbar-brand" href="/Home">MySchool</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="oi oi-menu"></span> Menu
+        </button>
+        <div className="collapse navbar-collapse" id="ftco-nav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item "><a href="/Home" className="nav-link pl-0">Home</a></li>
+            <li className="nav-item "><a href="/CoursesA" className="nav-link">Courses</a></li>
+            <li className="nav-item active"><a href="/TeachersA" className="nav-link">Teachers</a></li>
+            <li className="nav-item"><a href="/Pricing" className="nav-link">Pricing</a></li>
+            <li className="nav-item"><a href="/Contact" className="nav-link">Contact</a></li>
+            <li className="nav-item"><a href="#" className="nav-link" onClick={handleLogout}>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <h4 style={{ margin: "40px" }}>All Teachers</h4>
-  
-   
+    <Button variant="primary" onClick={handleModal}style={{ marginLeft: "40px" }}>
+      Add Teacher
+    </Button>
+    <table className="table" style={{ margin: "40px" }}>
+        <thead>
+          <tr>
+           <th>Id</th>
+           <th>Name</th>
+           <th>Surname</th>
+           <th>Description</th>
+           <th>Course</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Teachers.map(Teacher => (
+              <tr key={Teacher.id}>
+              <td>{Teacher.id}</td>
+              <td>{Teacher.name}</td>
+              <td>{Teacher.surname}</td>
+              <td>{Teacher.description}</td>
+              <td>{Teacher.courseId}</td>
+             <Button variant="warning"  onClick={() => handleEdit(Teacher.id)} style={{ marginRight: "15px" }}>
+               Edit
+             </Button>
+              <Button variant="danger"  onClick={() => handleDelete(Teacher.id)} style={{ marginLeft: "" }}>
+              Delete
+            </Button>
+            </tr>
+          ))}
+        </tbody>
+      </table>
      <Footer /> 
- 
+     <Modal show={showModal} onHide={handleModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add Teacher</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+  <input type="text" name="Id" value={newTeacher.Id} onChange={handleInputChange} hidden/>
+  <label>Name:</label><br/>
+  <input type="text" name="Name" value={newTeacher.Name} onChange={handleInputChange}/><br/>
+  <label>Surname:</label><br/>
+  <input type="text" name="Surname" value={newTeacher.Surname} onChange={handleInputChange}/><br/>
+  <label>Description:</label><br/>
+  <input type="text" name="Description" value={newTeacher.Description} onChange={handleInputChange}/><br/>
+  <label>Course:</label><br/>
+  <input type="text" name="Course" value={newTeacher.CourseId} onChange={handleInputChange}/><br/>
+ </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleModal}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleSaveTeacher}>
+          Save Teacher
+        </Button>
+      </Modal.Footer>
+    </Modal>
   </>
-     
   );
 };
 
 export default TeachersA;
-
-
-
